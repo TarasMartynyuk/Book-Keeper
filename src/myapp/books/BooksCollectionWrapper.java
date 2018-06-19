dab7e27ccaf81d9e913293220d6cce674d29038f
TarasMartynyuk <tarasmartynyuk22@gmail.com>
2018-06-19 22:50:48 +0300

    gitignore

--- src/myapp/books/BooksCollectionWrapper.java
+++ src/myapp/books/BooksCollectionWrapper.java
@@ -1,6 +1,7 @@
 package myapp.books;
 
 import com.mongodb.*;
+import myapp.Constants;
 
 import java.lang.reflect.Array;
 import java.net.UnknownHostException;
@@ -20,7 +21,6 @@ public class BooksCollectionWrapper {
         return _instance;
     }
 
-    private static final String BOOKS_DB_NAME = "Books";
     private static final String BOOKS_COLL_NAME = "books";
 
     private static final String USERNAME = "only_user";
@@ -33,12 +33,15 @@ public class BooksCollectionWrapper {
 
     private BooksCollectionWrapper() throws UnknownHostException {
 
-        var credential = MongoCredential.createCredential(USERNAME, BOOKS_DB_NAME, PASSWORD.toCharArray());
+        var credential = MongoCredential.createCredential(USERNAME,
+                Constants.BOOK_KEEPER_DB_NAME,
+                PASSWORD.toCharArray());
         _client = new MongoClient(new ServerAddress("localhost", 27017), Arrays.asList(credential));
 
+        var db = _client.getDB(Constants.BOOK_KEEPER_DB_NAME);
+        _booksColl = db.getCollection(BOOKS_COLL_NAME);
 
-        var booksDb = _client.getDB(BOOKS_DB_NAME);
-        _booksColl = booksDb.getCollection(BOOKS_COLL_NAME);
+        dropColl();
     }
 
     public void insert(DBObject dbObject) {
@@ -66,5 +69,9 @@ public class BooksCollectionWrapper {
             var obj = cursor.next();
             System.out.println('\t' + obj.toString());
         }
+    }
+
+    public void dropColl() {
+        _booksColl.drop();
     }
 }
